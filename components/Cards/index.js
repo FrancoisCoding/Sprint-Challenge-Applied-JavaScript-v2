9cf516c079e219c3eb81a4f14e61126bfbe65872
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-12 12:54:18 -0400

    Completed step 3 in cards component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,53 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardsContainer = document.querySelector(".cards-container");
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    var info = response.data.articles;
+    for (topic in info) {
+      const data = info[topic].forEach(obj =>
+        cardsContainer.appendChild(card(obj))
+      );
+    }
+  })
+  .catch(e => {
+    console.log("error", e);
+  });
+
+function card(data) {
+  // Created Card Div
+  var cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+
+  // Created Headline Div
+  var headlineDiv = document.createElement("div");
+  headlineDiv.classList.add("headline");
+  headlineDiv.textContent = data.headline;
+  cardDiv.appendChild(headlineDiv);
+
+  // Created Author Div
+  var authorDiv = document.createElement("div");
+  authorDiv.classList.add("author");
+  cardDiv.appendChild(authorDiv);
+
+  // Created Image Container Div
+  var imageContainerDiv = document.createElement("div");
+  imageContainerDiv.classList.add("img-container");
+  authorDiv.appendChild(imageContainerDiv);
+
+  // Created Image
+  var newImg = document.createElement("img");
+  newImg.src = data.authorPhoto;
+  imageContainerDiv.appendChild(newImg);
+
+  // Created Span
+  var span = document.createElement("span");
+  span.textContent = data.authorName;
+  authorDiv.appendChild(span);
+
+  // Append Div to Page
+  cardsContainer.appendChild(cardDiv);
+  return cardDiv;
+}
